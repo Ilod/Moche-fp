55c1b99c470d9827d22290332879a4c130ef98db
Ilod <rudy.pons@ilod.org>
2014-04-19 00:18:13 +0200

    Use submodules instead of svn repos for clang

--- utils/get_extern.bat
+++ utils/get_extern.bat
@@ -1,9 +1,10 @@
 %FILE_START% get_extern.bat
-%INTERNAL_CALL% svn.bat get llvm http://llvm.org/svn/llvm-project/llvm/trunk
-call svn.bat get llvm http://llvm.org/svn/llvm-project/llvm/trunk "LLVM"
-%INTERNAL_CALL% svn.bat get llvm\tools\clang http://llvm.org/svn/llvm-project/cfe/trunk
-call svn.bat get llvm\tools\clang http://llvm.org/svn/llvm-project/cfe/trunk "CLang"
-%INTERNAL_CALL% svn.bat get llvm\tools\clang\tools\extra http://llvm.org/svn/llvm-project/clang-tools-extra/trunk "CLang extras"
-call svn.bat get llvm\tools\clang\tools\extra http://llvm.org/svn/llvm-project/clang-tools-extra/trunk
-%INTERNAL_CALL% svn.bat get llvm\projects\compiler-rt http://llvm.org/svn/llvm-project/compiler-rt/trunk
-call svn.bat get llvm\projects\compiler-rt http://llvm.org/svn/llvm-project/compiler-rt/trunk "LLVM Compiler-rt"
+%PRE_EXTERNAL_CALL% mklink /J ..\submodules\llvm\tools\clang ..\submodules\clang 1>nul 2>nul
+%EXTERNAL_CALL% mklink /J ..\submodules\llvm\tools\clang ..\submodules\clang 1>nul 2>nul
+mklink /J ..\submodules\llvm\tools\clang ..\submodules\clang 1>nul 2>nul
+%PRE_EXTERNAL_CALL% mklink /J ..\submodules\llvm\tools\clang\tools\extra ..\submodules\clang-tools-extra 1>nul 2>nul
+%EXTERNAL_CALL% mklink /J ..\submodules\llvm\tools\clang\tools\extra ..\submodules\clang-tools-extra 1>nul 2>nul
+mklink /J ..\submodules\llvm\tools\clang\tools\extra ..\submodules\clang-tools-extra 1>nul 2>nul
+%PRE_EXTERNAL_CALL% mklink /J ..\submodules\llvm\projects\compiler-rt ..\submodules\compiler-rt 1>nul 2>nul
+%EXTERNAL_CALL% mklink /J ..\submodules\llvm\projects\compiler-rt ..\submodules\compiler-rt 1>nul 2>nul
+mklink /J ..\submodules\llvm\projects\compiler-rt ..\submodules\compiler-rt 1>nul 2>nul
